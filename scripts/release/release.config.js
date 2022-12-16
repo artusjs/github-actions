9d1b52f58ec2ba1234f34e448cd9cc6f23f9be7b
TZ <atian25@qq.com>
2022-12-16 16:52:39 +0800

    chore: remove config

--- scripts/release/release.config.js
+++ scripts/release/release.config.js
@@ -5,21 +5,11 @@ module.exports = {
     [ '@semantic-release/changelog', { changelogTitle: '# Changelog' } ],
     [ '@semantic-release/npm', {} ],
 
-    // [ '@semantic-release/exec', {
-    //   'publishCmd': 'ls -l dist && ls -l .semantic-release && tar -ztvf .semantic-release/*.tgz',
-    // } ],
-
     [ '@semantic-release/git',
       {
         message: 'Release <%= nextRelease.version %>\n\n[skip ci]\n\n<%= nextRelease.notes %>',
       },
     ],
-
-    [{
-      fail: function (...args) {
-        console.log('Hello World!', args);
-      }
-    }],
 
     [ '@semantic-release/github',
       {
